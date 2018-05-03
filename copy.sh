85a659abe8cdada502dc9effffa4abe992637d19
shawnvole <37572068+shawnvole@users.noreply.github.com>
2018-05-03 15:24:32 +1000

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+mkdir -p new
 n=$(wc -l < $1)
 i=1
 while [ $i -le $n ]
 do
       	x=$(sed -n "$i p" $1)
-	cp $x ~/data/new/ACST890_s1_2018_$x.txt
+	cp $x $PWD/new/ACST890_s1_2018_$x.txt
 	i=$(expr $i + 1)
 done
