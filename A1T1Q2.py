2b79382b13de21d628c8e250c27a9025973d59f1
shawnvole <37572068+shawnvole@users.noreply.github.com>
2018-05-04 18:05:18 +1000

    Create A1T1Q2.py

---
+++ A1T1Q2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import random
+
+x = random.randint(1,101)
+if x%2 == 0:
+    print('The result of this coin toss is Head')
+else:
+    print('The result of this coin toss is Tail')
